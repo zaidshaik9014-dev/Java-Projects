6a7b7cba871de3a4ad2c3612e91bed85aa706496
zaidshaik9014-dev <zaidshaik9014@gmail.com>
2026-07-21 14:49:21 +0530

    Modified the code

--- level 1/Password Generator/Main.java
+++ level 1/Password Generator/Main.java
@@ -2,72 +2,37 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int getValidChoice(Scanner sc) {
+    public static void generateAndDisplayPassword(
+            int length,
+            boolean upper,
+            boolean lower,
+            boolean numbers,
+            boolean symbols
+    ) {
 
-        while (true) { 
-
-            if (sc.hasNextInt()) {
-                int choice = sc.nextInt();
-
-                if (choice >= 1 && choice <= 3) {
-                    return choice;
-                }
-
-                System.out.println("Enter a number between 1 ans 3: ");
-
-            } else {
-
-                System.out.print("Invalid Input. \nEnter a number: ");
-
-                sc.next();
-
-            }
-        }
-    }
-
-    public static int getValidLength(Scanner sc) {
-
-        while (true) { 
+        try {
             
-            if (sc.hasNextInt()) {
+            String password = 
+                    PasswordGenerator.generatePassword(
+                        length, 
+                        upper, 
+                        lower, 
+                        numbers, 
+                        symbols
+                    );
 
-                int length = sc.nextInt();
+            System.out.println("\nGenerated Passwrod: " + password);
 
-                if (length >= 4 && length <= 64) {
-                    return length;
-                }
-            
-                System.out.print(
-                        "Password length must be between 4 and 64: "
-                );
-            } else {
+            System.out.println(
+                "Strength: "
+                            + PasswordStrength.checkStrength(password)
+            );
 
-                System.out.print("invalid Input.\nEnter a number: ");
+        } catch (IllegalArgumentException e) {
 
-                sc.next();
-                
-            }
+            System.out.println(e.getMessage());
         }
-    }
 
-    public static boolean getYesNo(Scanner sc, String message) {
-        
-        while (true) { 
-            
-            System.out.print(message);
-
-            String input = sc.next().trim();
-
-            if (input.equalsIgnoreCase("Y")) {
-                return true;
-            }
-
-            if (input.equalsIgnoreCase("N")) {
-                return false;
-            }
-
-            System.out.println("Please enter Y or N.");
-        }
     }
 
     public static void main(String[] args) {
@@ -78,67 +43,91 @@ public class Main {
             System.out.println("\n==============================");
             System.out.println(" PASSWORD GENERATOR ");
             System.out.println("==============================");
-            System.out.println("1. Generate Password");
-            System.out.println("2. Check Password Strength");
-            System.out.println("3. Exit");
+            System.out.println("1. Easy Password");
+            System.out.println("2. Medium Password");
+            System.out.println("3. Strong Password");
+            System.out.println("4. Custom Password");
+            System.out.println("5. Check Password Strength");
+            System.out.println("6. Exit");
             System.out.print("Enter Choice : ");
 
-            int choice = getValidChoice(sc);
+            int choice = InputHelper.getValidChoice(sc);
 
             switch (choice) {
 
                 case 1:
 
-                    System.out.print("Enter Password Length : ");
-                    int length = getValidLength(sc);
-
-                    boolean upper =
-                            getYesNo(sc,
-                            "Include Uppercase (Y/N): ");
-
-                    boolean lower =
-                            getYesNo(sc,
-                            "Include Lowercase (Y/N): ");
-
-                    boolean number =
-                            getYesNo(sc,
-                            "Include Numbers (Y/N): ");
-
-                    boolean symbol =
-                            getYesNo(sc,
-                            "Include Symbols (Y/N): ");
-
-                    try {
-                        
-                        String password = PasswordGenerator.generatePassword(
-                            length, upper, lower, number, symbol);
-
-                        System.out.println("\nGenerated Password : " + password);
-
-                        System.out.println(
-                            "Strength : " + PasswordStrength.checkStrength(password));
-
-                    } catch (IllegalArgumentException e) {
-                        System.out.println(e.getMessage());
-                    }
+                    generateAndDisplayPassword(
+                        8, 
+                        true, 
+                        true, 
+                        true,
+                        false
+                    );
 
                     break;
 
                 case 2:
 
-                    sc.nextLine();
-                    System.out.println("Enter Password : ");
-                    String password = sc.nextLine();
-                    System.out.println(
-                        "Strength : " + PasswordStrength.checkStrength(password));
-                    
+                    generateAndDisplayPassword(
+                        12,
+                        true,
+                        true,
+                        true,
+                        true
+                    );
+            
                     break;
 
                 case 3:
 
-                    System.out.println("Thank you!");
-                    sc.close();
-                    return;
+                    generateAndDisplayPassword(
+                        16,
+                        true,
+                        true,
+                        true,
+                        true
+                    );
+
+                    break;
+
+                case 4:
+
+                    int length = InputHelper.getValidLength(sc);
+
+                    boolean upper =
+                            InputHelper.getYesNo(
+                                    sc,
+                            "Include Uppercase (Y/N): "
+                    );
+
+                    boolean lower =
+                            InputHelper.getYesNo(
+                                    sc,
+                            "Include Lowercase (Y/N): "
+                    );
+
+                    boolean numbers =
+                            InputHelper.getYesNo(
+                                    sc,
+                            "Include Numbers (Y/N): "
+                    );
+
+                    boolean symbols =
+                            InputHelper.getYesNo(
+                                    sc,
+                            "Include Symbols (Y/N): "
+                    );
+
+                    generateAndDisplayPassword(
+                            length,
+                            upper,
+                            lower,
+                            numbers,
+                            symbols
+                    );
+
+                    break;
 
             }
         }
